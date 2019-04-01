72f2f0216f7dc34237fafc1bd3b4ea66745a1f55
saumpsh <saumpsh@gmail.com>
2019-04-01 11:27:44 -0700

    Basic Bluetooth Android App v1

---
+++ Android_App_Development/BluetoothFinder/app/src/main/java/com/example/bluetoothfinder/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.bluetoothfinder;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView statusTextView ;
+    Button searchButton;
+    BluetoothAdapter bluetoothAdapter;
+    ArrayList<String> bluetoothDevices = new ArrayList<>();
+    ArrayList<String> addresses = new ArrayList<>();
+    ArrayAdapter arrayAdapter ;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action", action);
+
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                statusTextView.setText("Finished");
+                searchButton.setEnabled(true);
+            }else if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE );
+                String name = device.getName();
+                String address = device.getAddress();
+                String rssi = Integer.toString( intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
+                Log.i("Device Found", "Name: " + name + " Address: "+ address + " RSSI:" + rssi);
+
+                if(!addresses.contains(address)){
+                    addresses.add(address);
+                    String deviceString = "";
+                    if(name == null || name.equals("")){
+                        deviceString = address + " - RSSI " + rssi + "dBm";
+                    }
+                    else{
+                        deviceString = name + " - RSSI " + rssi + "dBm";
+                    }
+                    bluetoothDevices.add(deviceString);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView1);
+        statusTextView = findViewById(R.id.textView1);
+        searchButton = findViewById(R.id.searchButton);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bluetoothDevices);
+
+        listView.setAdapter(arrayAdapter);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    public void searchClicked(View view){
+        statusTextView.setText("Searching...");
+        searchButton.setEnabled(false);
+        bluetoothDevices.clear();
+        addresses.clear();
+        bluetoothAdapter.startDiscovery();
+    }
+}
