827e32d5c60c46432204bc0980777d226836b4c2
Vatsal Makani <vatsalmakani@yahoo.com>
2019-05-08 19:41:41 -0700

    Deleting

--- Motor_Controller_Module/pre_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 
-# This file executes from the build directory by the Eclipse "Pre Build" step
-#
-
-python ../_can_dbc/dbc_parse.py -i ../_can_dbc/243_techSavvy.dbc -s MOTOR > ../_can_dbc/generated_can.h
